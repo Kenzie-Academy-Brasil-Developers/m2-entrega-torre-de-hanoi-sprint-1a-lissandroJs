1bdb27e2ca980f07ba36333738c16b27945182da
Leonardo Melegari Deffende <leo_melegari@outlook.com>
2021-11-30 09:02:17 -0300

    html refeito/script

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
+//chamar o elemento a ser clicado
+//usar o event listener para identificar o clique (tanto no disco, quanto na torre)
+
+
+
+
 // let disc1 = document.getElementById('disco1');
 // let disc2 = document.getElementById('disco2');
 // let disc3 = document.getElementById('disco3');
 // let disc4 = document.getElementById('disco4');
 
-const estado = document.querySelectorAll("[draggable='true']")
+const selectDisc = document.querySelectorAll("[draggable='true']");
 
-estado.forEach((es) => {
-    const selected = es.currentTarget; //armazena o clique
-    console.log("selected ", selected);
-    es.addEventListener('dragstart',selectElement);
-})
-
-function selectElement(event){
-    this.classList.add('selecionado')
+function selected(e){
+    const clicked = e.target;
+    console.log(`cliquei no ${e.target}`);
 }
+selectDisc.addEventListener('click', selected);
+
 
 // disc2.addEventListener('click',selectElement);
 // disc3.addEventListener('click',selectElement);
