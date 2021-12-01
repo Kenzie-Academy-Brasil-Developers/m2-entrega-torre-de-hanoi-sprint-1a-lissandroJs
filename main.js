70696941f8a88de959752cb6e293e2b242642ff2
Leonardo Melegari Deffende <leo_melegari@outlook.com>
2021-12-01 13:30:59 -0300

    projeto das torres finalizado

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
-
-/* INICIO DOM */
-let midSection = document.getElementById('middle');
+/*INICIO CAPTURA DOS ELEMENTOS GLOBAIS E DEFINIÇÕES*/
+const midSection = document.getElementById('middle'); //usada no appendChild do DOM - linha 44
+const selectAllTowers = document.getElementsByClassName('torre'); //Usada na função de movimentação - linha 66
+const getMain = document.getElementById('main'); //Usada no appendChild do button reset - linha 116
+const WinMsg = document.getElementById('vitoria'); //Usada para chamar a mensagem de vítoria na function verifyVictory() - linha 114
+//towers e disk precisaram ser preenchidas com "null", pois vão receber um objeto
+let towers = null;
+let disk = null;
+/*FIM CAPTURA DOS ELEMENTOS GLOBAIS E DEFINIÇÕES*/
 
 let divTorre1 = document.createElement('div');
 let divTorre2 = document.createElement('div');
 let divTorre3 = document.createElement('div');
-let disc1 = document.createElement('div');
-let disc2 = document.createElement('div');
-let disc3 = document.createElement('div');
-let disc4 = document.createElement('div');
+let disk1 = document.createElement('div');
+let disk2 = document.createElement('div');
+let disk3 = document.createElement('div');
+let disk4 = document.createElement('div');
 
-disc1.setAttribute('id', '4');
-disc1.setAttribute('class', 'disco disco-vermelho');
+disk1.setAttribute('id', '4');
+disk1.setAttribute('class', 'disco disco-vermelho');
 
-disc2.setAttribute('id', '3');
-disc2.setAttribute('class', 'disco disco-verde');
+disk2.setAttribute('id', '3');
+disk2.setAttribute('class', 'disco disco-verde');
 
-disc3.setAttribute('id', '2');
-disc3.setAttribute('class', 'disco disco-azul');
+disk3.setAttribute('id', '2');
+disk3.setAttribute('class', 'disco disco-azul');
 
-disc4.setAttribute('id', '1');
-disc4.setAttribute('class', 'disco disco-roxo');
+disk4.setAttribute('id', '1');
+disk4.setAttribute('class', 'disco disco-roxo');
 
 divTorre1.setAttribute('id', 'torre1');
 divTorre1.setAttribute('class', 'torre torre1');
@@ -31,48 +37,169 @@ divTorre2.setAttribute('class', 'torre torre2');
 divTorre3.setAttribute('id', 'torre3');
 divTorre3.setAttribute('class', 'torre torre3');
 
-
-divTorre1.appendChild(disc1);
-divTorre2.appendChild(disc2);
-divTorre3.appendChild(disc3);
-divTorre1.appendChild(disc4);
+divTorre1.appendChild(disk1);
+divTorre1.appendChild(disk2);
+divTorre1.appendChild(disk3);
+divTorre1.appendChild(disk4);
 
 midSection.appendChild(divTorre1);
 midSection.appendChild(divTorre2);
 midSection.appendChild(divTorre3);
-/* FIM DOM */
 
-// CONDIÇÃO DE VITORIA - COMEÇO - falta testar
-let getMain = document.getElementById('main')
-if(divTorre1.childElementCount === 2){
-    let msgVitoria = document.createElement('span');
-    let btnReset = document.createElement('button');
-
-    btnReset.textContent = "Recomeçar";
-    msgVitoria.innerText = "Parabéns, você ganhou!";
-
-    msgVitoria.setAttribute('class','vitoria');
-    btnReset.setAttribute('class','btnReset');
-    btnReset.setAttribute('id','btnReset')
-
-    getMain.appendChild(msgVitoria);
-    getMain.appendChild(btnReset);
+//seleção de torre
+function selectedTower(event) {
+    //ident e armaz torre clicada
+    let clickedTower = event.currentTarget;
+    //retorna a torre clicada para ser usado na função de movimentar o disco
+    return clickedTower;
 }
-//CONDIÇÃO DE VITORIA - FIM
 
-// COMEÇO BOTAO RESETAR - já funcional
-let btn = document.getElementById('btnReset')
-function btnResetar(event){
+//seleção de disco
+function selectedDisc(event) {
+    //identifica e armazena em qual torre estou
+    let clickedTower = event.currentTarget;
+    //identifica o ultimo elemento na torre clicada e armazena
+    let clickedDisc = clickedTower.lastElementChild;
+
+    //retorno o valor obtido acima para ser usado na função de movimentar o disco
+    return clickedDisc;
+}
+
+//função de movimentação dos discos
+function diskMovement() {
+    for (let index = 0; index < selectAllTowers.length; index++) {
+
+        let towers = selectAllTowers[index];
+        //aplico o eventlistener para cada indice percorrido no for
+        towers.addEventListener('click', function(event){
+            towers = selectedTower(event); //peguei o evento do clique retornado na função selectedTower
+            //preciso verificar quantos filhos tem nessa torre recebida/clicada
+            let verifyChild = towers.childElementCount;//retorna um number
+            //condição de seleção do disco
+            if (disk === null && verifyChild > 0) 
+            { //se verdadeiro, armazenar o disco atual em disk e add classe
+                disk = selectedDisc(event);
+                disk.classList.add('selecionado-disco');
+            }
+            else if (verifyChild > 0) 
+            {
+                let child = selectedDisc(event);
+                
+                let diskSize = disk.clientWidth; // pega o disco armazenado na variavel global, pega o tamanho dela e armazena
+
+                let childSize = child.clientWidth; // pega o disco da torre atual, pega o tamanho e armazena
+
+                if (diskSize < childSize) //verifico os tamanhos
+                {
+                    towers.appendChild(disk);
+                    disk.classList.remove('selecionado-disco');
+                    disk = null;
+                    towers = null;
+                }
+                else 
+                {
+                    disk.classList.remove('selecionado-disco')
+                    disk = null;
+                    towers = null;
+                }
+            } 
+            else if (disk != null)
+            {
+                towers.appendChild(disk);
+                disk.classList.remove('selecionado-disco');
+                disk = null;
+                torre = null;
+            }
+            verifyVictory()
+        })
+    }
+}
+diskMovement();
+
+function verifyVictory() {
+    if (divTorre3.childElementCount == 4) 
+    {
+        let msgVitoria = document.createElement('span');
+
+        msgVitoria.innerText = "Parabéns, você ganhou!";
+        msgVitoria.setAttribute('class', 'vitoria');
+
+        getMain.appendChild(msgVitoria);
+        setTimeout(function () {
+            msgVitoria.innerText = "";
+        }, 3000)
+    }
+    return;
+}
+
+
+//botão de reset
+let btnReset = document.createElement('button');
+btnReset.setAttribute('class', 'btnReset');
+btnReset.setAttribute('id', 'btnReset')
+btnReset.textContent = "Recomeçar";
+
+getMain.appendChild(btnReset);
+const btn = document.getElementById('btnReset') //Usada para chamar o botão de reset criado - linha 132
+
+
+function btnResetar(event) {
     const reset = event.target;
-    if(reset.tagName == 'BUTTON'){
-        divTorre1.appendChild(disc1);
-        divTorre1.appendChild(disc2);
-        divTorre1.appendChild(disc3);
-        divTorre1.appendChild(disc4);
+    // if(reset.tagName == 'BUTTON' && )
+    if (reset.tagName == 'BUTTON') 
+    {
+        divTorre1.appendChild(disk1);
+        divTorre1.appendChild(disk2);
+        divTorre1.appendChild(disk3);
+        divTorre1.appendChild(disk4);
+        // if(WinMsg.value !== ""){
+        //     WinMsg.innerText = "";
+        // }
     }
 }
 btn.addEventListener('click', btnResetar);
-//FIM BOTAO RESETAR
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -88,36 +215,28 @@ btn.addEventListener('click', btnResetar);
 
 // //SELECIONANDO OS ALVOS
 
-// //disco
-// let getDisc1 = document.getElementById('4');
-// let getDisc2 = document.getElementById('3');
-// let getDisc3 = document.getElementById('2');
-// let getDisc4 = document.getElementById('1');
 // //vareta
-// let getTorre1 = document.getElementById('torre1')
-// let getTorre2 = document.getElementById('torre2')
-// let getTorre3 = document.getElementById('torre3')
-
-// function selectedDisc(event) {
-//     const clicked = event.target; // capturo o clique
-//     let id = Number(event.target.id);//capturo o ID do disco clicado
-
-//     if (clicked.tagName == 'DIV') {
-//         clicked.classList.toggle('selecionado-disco');
-//     }
-
-// }
+// let getTorre1 = document.getElementById('torre1');
+// let getTorre2 = document.getElementById('torre2');
+// let getTorre3 = document.getElementById('torre3');
 
 // function selectedVareta(event) {
-//     const selectedTorre = event.currentTarget;
+//     const selectedTorre = event.currentTarget; // armazeno qual torre foi clicada
 //     let quantos = selectedTorre.childElementCount; // retorna quantos elementos tem na vareta
+//     let count = selectedTorre.lastElementChild; //pega o elemento de cima da torre
+//     let width = count.clientWidth; //pega o tamanho do elemento armazenado em count
+//     moveTower(event)
+// }
+// function moveTower(event){
+
+
 // }
 
-// getDisc1.addEventListener('click', selectedDisc);
-// getDisc2.addEventListener('click', selectedDisc);
-// getDisc3.addEventListener('click', selectedDisc);
-// getDisc4.addEventListener('click', selectedDisc);
 
 // getTorre1.addEventListener('click', selectedVareta)
 // getTorre2.addEventListener('click', selectedVareta)
 // getTorre3.addEventListener('click', selectedVareta)
+
+// getTorre1.addEventListener('click', moveTower)
+// getTorre2.addEventListener('click', moveTower)
+// getTorre3.addEventListener('click', moveTower)
